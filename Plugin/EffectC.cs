d4679f5548391cb6306571d2134bb62ce858fa87
Walter Cain <30776780+walterhcain@users.noreply.github.com>
2026-03-12 10:08:59 -0700

    Add files via upload

---
+++ Plugin/EffectC.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace walterhcain.EffectTester
+{
+    public class EffectC : IRocketPluginConfiguration
+    {
+        public string DontNeed;
+        public void LoadDefaults()
+        {
+            DontNeed = "Nothing";
+        }
+    }
+}
---
+++ Plugin/EffectP.cs
@@ -0,0 +1,23 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+
+namespace walterhcain.EffectTester
+{
+    public class EffectP : RocketPlugin<EffectC>
+    {
+        public static EffectP Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("Cain's Effect Tester has been successfully loaded");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("Cain's Effect Tester has been successfully unloaded");
+        }
+    }
+}
